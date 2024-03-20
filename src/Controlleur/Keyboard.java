054f41bd7f3dc5846226d92408082a8f360497b4
duybeobn1 <duybeobn1@gmail.com>
2024-03-20 11:41:31 +0100

    Update Keyboard.java

--- src/Controlleur/Keyboard.java
+++ src/Controlleur/Keyboard.java
@@ -1,6 +1,8 @@
 package Controlleur;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+import org.w3c.dom.events.Event;
 public class Keyboard implements KeyListener {
     private boolean[] keys;
     public static boolean UP, LEFT, RIGHT, DOWN;
@@ -13,21 +15,24 @@ public class Keyboard implements KeyListener {
         DOWN = false;
     }
 
+    public void update() {
+        UP = keys[KeyEvent.VK_UP];
+        LEFT = keys[KeyEvent.VK_LEFT];
+        RIGHT = keys[KeyEvent.VK_RIGHT];
+        DOWN = keys[KeyEvent.VK_DOWN];
+    }
     @Override
     public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+        //unused
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        keys[e.getKeyCode()] = true;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+        keys[e.getKeyCode()] = false;
     }
 }
